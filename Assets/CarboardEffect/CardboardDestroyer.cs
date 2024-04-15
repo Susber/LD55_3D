47a6284e6dba0aad458acb1255e41afd9ab62627
anton.florey <anton.florey@googlemail.com>
2024-04-15 15:54:33 +0200

    better cardboard rendering

---
+++ Assets/CarboardEffect/CardboardDestroyer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CardboardDestroyer : MonoBehaviour
+{
+    public GameObject cardboardPiece;
+
+    private float destroytimer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destroytimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destroytimer += Time.deltaTime;
+        if (destroytimer > 2)
+        {
+			Collider myCollider = GetComponent<Collider>();
+			myCollider.enabled = false;
+			SpawnDestroyedCardboard(10);
+			Destroy(transform.gameObject);
+		}
+    }
+
+    public void SpawnDestroyedCardboard(int targetNumPieces)
+    {
+        // Get object with correct mesh
+        var frontFaceChild = transform.Find("CardboardFace");
+        if (frontFaceChild == null)
+        {
+            Debug.Log("No Cardboard to destroy");
+            return;
+        }
+        Mesh cutMesh = frontFaceChild.GetComponent<MeshFilter>().mesh;
+        // Translate uvs from sprite mesh to the mesh to be cut TODO
+        var mySpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        if (mySpriteRenderer == null)
+        {
+            Debug.Log("No sprite to render for destroyed cardboard!");
+            return;
+        }
+        // cut the mesh
+        List<Mesh> pieces = CutterMesh.RecursiveCutting(cutMesh, targetNumPieces, new Vector3(0, 0, 1));
+
+        // create game objects for each cut mesh
+            // cardboard, texture front and backface, colliders
+        
+
+
+    }
+
+}
