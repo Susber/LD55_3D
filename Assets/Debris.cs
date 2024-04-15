62a738c1e1f6226c5e03668136519bbfb22fa305
anton.florey <anton.florey@googlemail.com>
2024-04-15 23:22:09 +0200

    it focking works :O

---
+++ Assets/Debris.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debris : MonoBehaviour
+{
+	float myTimer = 0;
+	float explosionTime = 3;
+	float selfEndTime = 6;
+	bool exploded = false;
+	bool deleted = false;
+
+	private void Update()
+	{
+		myTimer += Time.deltaTime;
+		if (myTimer > explosionTime && !exploded)
+		{
+			exploded = true;
+			// delete all children
+			for (int i = transform.childCount - 1; i >= 0; i--)
+			{
+				Destroy(transform.GetChild(i).gameObject);
+			}
+			// explosion TODO
+		}
+
+		if (myTimer > selfEndTime && !deleted)
+		{
+			deleted = true;
+			// boom
+			GameObject.Destroy(transform.gameObject);
+		}
+	}
+
+	private void Awake()
+	{
+		myTimer = 0; 		
+	}
+}
