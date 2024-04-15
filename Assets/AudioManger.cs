39d5920fda3dfcd20f828d8d0bc08ff91b227ebf
Sukram28 <sukram2a@web.de>
2024-04-15 15:42:17 +0200

    Added Audio Manager

---
+++ Assets/AudioManger.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManger : MonoBehaviour
+{
+    public static AudioManger Instance;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+    public float volume = 0.5f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlaySoundGun();
+        }
+    }
+
+    void PlaySoundGun()
+    {
+        PlaySound(0, 0.2f);
+    }
+
+
+    // Function to play a specific sound from the array
+    public void PlaySound(int index, float volume)
+    {
+        // Check if the index is within the bounds of the array
+        if (index < 0 || index >= audioClips.Length) return;
+
+        // Create a temporary GameObject for playing the sound
+        GameObject soundGameObject = new GameObject("TempAudio");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+
+        // Assign the clip from the array based on the index
+        audioSource.clip = audioClips[index];
+        audioSource.volume = volume;
+        audioSource.Play();
+
+        // Destroy the GameObject after the clip finishes playing
+        Destroy(soundGameObject, audioClips[index].length);
+    }
+}
